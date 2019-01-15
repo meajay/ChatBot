2e5b147e55fbca8e8e5553a3a926f2ae4b458f36
MEAJAY <ACHAUHAN142@GMAIL.COM>
2019-01-15 17:54:28 +0530

    Project created

---
+++ app/src/androidTest/java/chatbot/assignment/com/chatbot/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package chatbot.assignment.com.chatbot;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("chatbot.assignment.com.chatbot", appContext.getPackageName());
+    }
+}
